907aee1695934478c9df9f09c1db7f0f1cbdf67d
CodeShangDS <shangxhyyds@gmail.com>
2022-10-18 23:01:40 +0800

    初识关键字 typedef static

---
+++ text_10_15/text_10_15/text_10_15.c
@@ -0,0 +1,45 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+////下标引用操作符；
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	printf("%d\n", arr[5]);
+//	return 0;
+//}
+//int main()
+//{
+//	//大量频繁被使用的数据，想放在寄存器中，提升效率；
+//	register int num = 100;//建议num的值存放到寄存器中去；
+//	return 0;
+//}
+
+//typedef 类型定义
+//类型重命名
+
+//typedef unsigned int u_int;
+//int main()
+//{
+//	unsigned int num = 100;
+//	u_int num2 = 200;
+//	return 0;
+//}
+
+//static 静态的 
+
+void test()
+{
+	static int a = 1;
+	a++;
+	printf("%d ", a);
+}
+int main()
+{
+	int i = 0;
+	while (i < 10)
+	{
+		test();
+		i++;		
+	}
+	return 0;
+}
